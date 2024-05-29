d08ec3f863a4910f553dd3ebc47d1a8e5bc6ceac
jwalberg <princewally@gmail.com>
2024-05-29 13:09:12 -0500

    Update media_player.py

--- media_player.py
+++ media_player.py
@@ -1,4 +1,4 @@
-"""Support to interface with the AIMP Web API."""
+"""Support to interface with the hadora Web API."""
 import logging
 import re
 
@@ -38,10 +38,10 @@ from homeassistant.util import Throttle
 
 _LOGGER = logging.getLogger(__name__)
 
-DEFAULT_NAME = "AIMP"
+DEFAULT_NAME = "hadora"
 DEFAULT_PORT = 3333
 
-SUPPORT_AIMP = (
+SUPPORT_hadora = (
     SUPPORT_PAUSE
     | SUPPORT_VOLUME_SET
     | SUPPORT_VOLUME_MUTE
@@ -66,15 +66,15 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 )
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Set up the AIMP platform."""
+    """Set up the hadora platform."""
     name = config.get(CONF_NAME)
     host = config.get(CONF_HOST)
     port = config.get(CONF_PORT)
 
-    add_entities([AIMP(name, host, port, hass)], True)
+    add_entities([HAdora(name, host, port, hass)], True)
 
-class AIMP(MediaPlayerEntity):
-    """AIMP Player Object."""
+class HAdora(MediaPlayerEntity):
+    """HAdora Player Object."""
 
     def __init__(self, name, host, port, hass):
         """Initialize the media player."""
@@ -85,7 +85,7 @@ class AIMP(MediaPlayerEntity):
         self._name = name
         self._available = False
 
-        self._aimp = None
+        self._hadora = None
         self._coverurl = {}
         self._playinfo = {}
         self._playlists = []
@@ -101,7 +101,7 @@ class AIMP(MediaPlayerEntity):
         self._shuffle = {}
         self._is_volume_muted = {}
 
-    def send_aimp_msg(self, method, params):
+    def send_hadora_msg(self, method, params):
         """Send message."""
         try:
             url = f"http://{self._url}/RPC_JSON"
@@ -123,7 +123,7 @@ class AIMP(MediaPlayerEntity):
 
         except requests.exceptions.RequestException:
             _LOGGER.error(
-                "Could not send command %s to AIMP at: %s", method, self._url
+                "Could not send command %s to hadora at: %s", method, self._url
             )
             return False
         try:
@@ -134,7 +134,7 @@ class AIMP(MediaPlayerEntity):
 
     def update(self):
         """Update state."""
-        if self._aimp is None:
+        if self._hadora is None:
             try:
                 url = f"http://{self._url}/RPC_JSON"
                 payload = {"version":"1.1","method":"Status","id":1,"params":{"status_id":4}}
@@ -153,23 +153,23 @@ class AIMP(MediaPlayerEntity):
             except requests.exceptions.RequestException:
                 if self._available:
                     _LOGGER.error(
-                        "Connect error to AIMP at: %s", self._url
+                        "Connect error to hadora at: %s", self._url
                     )
                     self._available = False
-                self._aimp = None
+                self._hadora = None
                 return
 
             self._state = STATE_IDLE
             self._available = True
 
     def update_playinfo(self):
-        resp = self.send_aimp_msg("GetPlaylistEntryInfo", {"playlist_id": -1, "track_id": -1})
+        resp = self.send_hadora_msg("GetPlaylistEntryInfo", {"playlist_id": -1, "track_id": -1})
         if resp is False:
             return
         self._playinfo = resp.copy()
 
     def update_coverurl(self):
-        resp = self.send_aimp_msg("GetCover", {"playlist_id": -1, "track_id": -1, "cover_width": 512, "cover_height": 512})
+        resp = self.send_hadora_msg("GetCover", {"playlist_id": -1, "track_id": -1, "cover_width": 512, "cover_height": 512})
         if resp is False:
             return
         elif resp is None:
@@ -178,31 +178,31 @@ class AIMP(MediaPlayerEntity):
             self._coverurl = resp.copy()
 
     def update_state(self):
-        resp = self.send_aimp_msg("Status", {"status_id":4})
+        resp = self.send_hadora_msg("Status", {"status_id":4})
         if resp is False:
             return
         self._state_dict = resp.copy()
 
     def update_media_position(self):
-        resp = self.send_aimp_msg("Status", {"status_id":31})
+        resp = self.send_hadora_msg("Status", {"status_id":31})
         if resp is False:
             return
         self._media_position_dict = resp.copy()
 
     def update_volume_level(self):
-        resp = self.send_aimp_msg("Status", {"status_id":1})
+        resp = self.send_hadora_msg("Status", {"status_id":1})
         if resp is False:
             return
         self._volume_level = resp.copy()
 
     def update_shuffle(self):
-        resp = self.send_aimp_msg("Status", {"status_id":41})
+        resp = self.send_hadora_msg("Status", {"status_id":41})
         if resp is False:
             return
         self._shuffle = resp.copy()
 
     def update_is_volume_muted(self):
-        resp = self.send_aimp_msg("Status", {"status_id":5})
+        resp = self.send_hadora_msg("Status", {"status_id":5})
         if resp is False:
             return
         self._is_volume_muted = resp.copy()
@@ -316,45 +316,45 @@ class AIMP(MediaPlayerEntity):
     @property
     def supported_features(self):
         """Flag of media commands that are supported."""
-        return SUPPORT_AIMP
+        return SUPPORT_hadora
 
     def media_next_track(self):
         """Send media_next command to media player."""
-        self.send_aimp_msg("PlayNext", "{}")
+        self.send_hadora_msg("PlayNext", "{}")
 
     def media_previous_track(self):
         """Send media_previous command to media player."""
-        self.send_aimp_msg("PlayPrevious", "{}")
+        self.send_hadora_msg("PlayPrevious", "{}")
 
     def media_play(self):
         """Send media_play command to media player."""
-        self.send_aimp_msg("Play", "{}")
+        self.send_hadora_msg("Play", "{}")
 
     def media_pause(self):
         """Send media_pause command to media player."""
-        self.send_aimp_msg("Pause", "{}")
+        self.send_hadora_msg("Pause", "{}")
 
     def media_stop(self):
         """Send media_pause command to media player."""
-        self.send_aimp_msg("Stop", "{}")
+        self.send_hadora_msg("Stop", "{}")
 
     def set_volume_level(self, volume):
         """Send volume_up command to media player."""
-        self.send_aimp_msg("Status", {"status_id": 1, "value": int(volume * 100)})
+        self.send_hadora_msg("Status", {"status_id": 1, "value": int(volume * 100)})
 
     def mute_volume(self, mute):
         """Send mute command to media player."""
         mutecmd = 1 if mute else 0
-        self.send_aimp_msg("Status", {"status_id": 5, "value": mutecmd })
+        self.send_hadora_msg("Status", {"status_id": 5, "value": mutecmd })
    
     def set_shuffle(self, shuffle):
         """Enable/disable shuffle mode."""
         shufflecmd = 1 if shuffle else 0
-        self.send_aimp_msg("Status", {"status_id": 5, "value": shufflecmd })
+        self.send_hadora_msg("Status", {"status_id": 5, "value": shufflecmd })
         
     def media_seek(self, position):
         """Send seek command."""
-        self.send_aimp_msg("Status", {"status_id": 31, "value": int(position) })
+        self.send_hadora_msg("Status", {"status_id": 31, "value": int(position) })
 
     def select_source(self, source):
         """Choose a different available playlist and play it."""
@@ -367,7 +367,7 @@ class AIMP(MediaPlayerEntity):
             return "key doesn't exist"
 
         playlistid = get_key(source)
-        resp = self.send_aimp_msg("GetPlaylistEntries", {"playlist_id":int(playlistid),"fields":["album","artist","bitrate","genre","duration","filesize","date","id","rating"]})
+        resp = self.send_hadora_msg("GetPlaylistEntries", {"playlist_id":int(playlistid),"fields":["album","artist","bitrate","genre","duration","filesize","date","id","rating"]})
         try: 
             entries = resp.get("entries")
         except:
@@ -386,19 +386,19 @@ class AIMP(MediaPlayerEntity):
             _LOGGER.error("Received invalid response: trackinfo: %s", trackinfo,)
             return False
 
-        resp2 = self.send_aimp_msg("Play", {"playlist_id": int(playlistid), "track_id": int(trackid)})
+        resp2 = self.send_hadora_msg("Play", {"playlist_id": int(playlistid), "track_id": int(trackid)})
         if resp2 is False:
             return
         
     def clear_playlist(self):
         """Clear players playlist."""
         self._currentplaylist = None
-        self.send_aimp_msg("Stop", "{}")
+        self.send_hadora_msg("Stop", "{}")
 
     @Throttle(PLAYLIST_UPDATE_INTERVAL)
     def update_playlists(self, **kwargs):
-        """Update available AIMP playlists."""
-        playlists = self.send_aimp_msg(
+        """Update available hadora playlists."""
+        playlists = self.send_hadora_msg(
             "GetPlaylists", "{fields: [\"id\", \"title\", \"crc32\"]}}"
         )
         self._playlists = re.findall(r"'title': '(.+?)'", str(playlists))
